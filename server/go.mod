2ffe395ae95af1bbb5edf75c044c32736fc16b6b
Ben Coleman <benc.uk@gmail.com>
2019-08-21 14:57:28 +0100

    Small fixes

--- server/go.mod
+++ server/go.mod
@@ -6,5 +6,5 @@ require (
 	github.com/benc-uk/go-starter v1.0.0
 	github.com/gorilla/mux v1.7.3
 	github.com/shirou/gopsutil v2.18.12+incompatible
-	golang.org/x/sys v0.0.0-20190614160838-b47fdc937951 // indirect
+	golang.org/x/sys v0.0.0-20190813064441-fde4db37ae7a // indirect
 )
--- server/main.go
+++ server/main.go
@@ -12,7 +12,6 @@ var contentDir string
 var serverPort string
 
 func main() {
-
 	// Get server PORT setting or default
 	serverPort := envhelper.GetEnvString("PORT", "4000")
 	// Get CONTENT_DIR setting for static content or default
@@ -21,6 +20,13 @@ func main() {
 	// API keys
 	darkskyAPIKey := envhelper.GetEnvString("WEATHER_API_KEY", "") 
 	ipstackAPIKey := envhelper.GetEnvString("IPSTACK_API_KEY", "") 
+
+	if(len(ipstackAPIKey) > 0) {
+		fmt.Println("### Weather API enabled with DarkSky API key")
+	}
+	if(len(ipstackAPIKey) > 0) {
+		fmt.Println("### Weather API enabled with IPStack API key")
+	}
 
 	// Routing
 	muxrouter := mux.NewRouter()
