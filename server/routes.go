3901cee098d9387c047651558d309f938205b741
Ben Coleman <benc.uk@gmail.com>
2020-11-08 13:35:32 +0000

    Much better error handling from API

--- server/routes.go
+++ server/routes.go
@@ -47,6 +47,11 @@ type Weather struct {
 	WeatherInfo darkskyAPIData `json:"weather"`
 }
 
+// HTTPError holds API JSON error
+type HTTPError struct {
+	Error string `json:"error"`
+}
+
 // ipstackAPIData holds results of IPStack API call
 type ipstackAPIData struct {
 	City    string  `json:"city"`
@@ -85,6 +90,7 @@ func (r Routes) apiInfoRoute(resp http.ResponseWriter, req *http.Request) {
 	if r.disableCORS {
 		resp.Header().Set("Access-Control-Allow-Origin", "*")
 	}
+	resp.Header().Set("Content-Type", "application/json")
 
 	var info SysInfo
 
@@ -101,12 +107,11 @@ func (r Routes) apiInfoRoute(resp http.ResponseWriter, req *http.Request) {
 	// JSON-ify our info
 	js, err := json.Marshal(info)
 	if err != nil {
-		http.Error(resp, err.Error(), http.StatusInternalServerError)
+		apiError(resp, http.StatusInternalServerError, err.Error())
 		return
 	}
 
 	// Fire JSON result back down the internet tubes
-	resp.Header().Set("Content-Type", "application/json")
 	resp.Write(js)
 }
 
@@ -118,13 +123,14 @@ func (r Routes) apiMetricsRoute(resp http.ResponseWriter, req *http.Request) {
 	if r.disableCORS {
 		resp.Header().Set("Access-Control-Allow-Origin", "*")
 	}
+	resp.Header().Set("Content-Type", "application/json")
 
 	var metrics Metrics
 
 	// Memory stuff
 	memStats, err := mem.VirtualMemory()
 	if err != nil {
-		http.Error(resp, err.Error(), http.StatusInternalServerError)
+		apiError(resp, http.StatusInternalServerError, "Virtual memory "+err.Error())
 		return
 	}
 	metrics.MemTotal = memStats.Total
@@ -133,7 +139,7 @@ func (r Routes) apiMetricsRoute(resp http.ResponseWriter, req *http.Request) {
 	// CPU / processor stuff
 	cpuStats, err := cpu.Percent(0, false)
 	if err != nil {
-		http.Error(resp, err.Error(), http.StatusInternalServerError)
+		apiError(resp, http.StatusInternalServerError, "CPU percentage "+err.Error())
 		return
 	}
 	metrics.CPUPerc = cpuStats[0]
@@ -141,7 +147,7 @@ func (r Routes) apiMetricsRoute(resp http.ResponseWriter, req *http.Request) {
 	// Disk and filesystem usage stuff
 	diskStats, err := disk.Usage("/")
 	if err != nil {
-		http.Error(resp, err.Error(), http.StatusInternalServerError)
+		apiError(resp, http.StatusInternalServerError, "Disk usage "+err.Error())
 		return
 	}
 	metrics.DiskTotal = diskStats.Total
@@ -150,7 +156,7 @@ func (r Routes) apiMetricsRoute(resp http.ResponseWriter, req *http.Request) {
 	// Network stuff
 	netStats, err := net.IOCounters(false)
 	if err != nil {
-		http.Error(resp, err.Error(), http.StatusInternalServerError)
+		apiError(resp, http.StatusInternalServerError, "IOCounters "+err.Error())
 		return
 	}
 	metrics.NetBytesRecv = netStats[0].BytesRecv
@@ -159,12 +165,11 @@ func (r Routes) apiMetricsRoute(resp http.ResponseWriter, req *http.Request) {
 	// JSON-ify our metrics
 	js, err := json.Marshal(metrics)
 	if err != nil {
-		http.Error(resp, err.Error(), http.StatusInternalServerError)
+		apiError(resp, http.StatusInternalServerError, err.Error())
 		return
 	}
 
 	// Fire JSON result back down the internet tubes
-	resp.Header().Set("Content-Type", "application/json")
 	resp.Write(js)
 }
 
@@ -182,23 +187,31 @@ func (r Routes) weatherRoute(resp http.ResponseWriter, req *http.Request) {
 	if r.disableCORS {
 		resp.Header().Set("Access-Control-Allow-Origin", "*")
 	}
+	resp.Header().Set("Content-Type", "application/json")
 
-	// Top level JSON container object
+	// Top level JSON container struct
 	var weather Weather
 
-	// Try to guess calling IP address
+	// Try to deduce calling IP address
 	ip := req.Header.Get("x-forwarded-for")
-	//ip = "86.134.117.146"
+	ip = "86.134.117.146"
+
+	// If not in the header try the RemoteAddr field
 	if len(ip) == 0 {
 		ip = req.RemoteAddr
 	}
+
+	// Checks for localhost, as it won't work
 	if strings.HasPrefix(ip, "127.0.0.1") || strings.HasPrefix(ip, "[::1]") {
-		http.Error(resp, "Localhost not allowed", http.StatusNotAcceptable)
+		apiError(resp, http.StatusNotAcceptable, fmt.Sprintf("This IP is not allowed %v", ip))
 		return
 	}
+
+	// There might be a port in there, get rid of it
 	if strings.Contains(ip, ":") {
 		ip = strings.Split(ip, ":")[0]
 	}
+
 	weather.IPAddress = ip
 
 	// First API call is to IPStack to reverse lookup IP into location (lat & long)
@@ -210,7 +223,7 @@ func (r Routes) weatherRoute(resp http.ResponseWriter, req *http.Request) {
 	url := fmt.Sprintf("http://api.ipstack.com/%s?access_key=%s&format=1", ip, r.ipstackAPIKey)
 	apiresponse, err := netClient.Get(url)
 	if err != nil {
-		http.Error(resp, err.Error(), http.StatusInternalServerError)
+		apiError(resp, http.StatusInternalServerError, err.Error())
 		return
 	}
 	body, err := ioutil.ReadAll(apiresponse.Body)
@@ -219,11 +232,11 @@ func (r Routes) weatherRoute(resp http.ResponseWriter, req *http.Request) {
 	var ipstackData ipstackAPIData
 	err = json.Unmarshal(body, &ipstackData)
 	if err != nil {
-		http.Error(resp, err.Error(), http.StatusInternalServerError)
+		apiError(resp, http.StatusInternalServerError, err.Error())
 		return
 	}
 	if len(ipstackData.City) == 0 {
-		http.Error(resp, fmt.Sprintf("{\"msg\": \"Error no data for IP %v\"}", ip), http.StatusInternalServerError)
+		apiError(resp, http.StatusNotFound, fmt.Sprintf("No weather data for this IP %v", ip))
 		return
 	}
 	weather.GeoInfo = ipstackData
@@ -232,7 +245,7 @@ func (r Routes) weatherRoute(resp http.ResponseWriter, req *http.Request) {
 	url = fmt.Sprintf("https://api.darksky.net/forecast/%s/%v,%v?exclude=minutely,hourly,daily&units=si", r.darkskyAPIKey, weather.GeoInfo.Lat, weather.GeoInfo.Long)
 	apiresponse, err = netClient.Get(url)
 	if err != nil {
-		http.Error(resp, err.Error(), http.StatusInternalServerError)
+		apiError(resp, http.StatusInternalServerError, err.Error())
 		return
 	}
 	body, err = ioutil.ReadAll(apiresponse.Body)
@@ -241,18 +254,38 @@ func (r Routes) weatherRoute(resp http.ResponseWriter, req *http.Request) {
 	var darkskyData darkskyAPIData
 	err = json.Unmarshal(body, &darkskyData)
 	if err != nil {
-		http.Error(resp, err.Error(), http.StatusInternalServerError)
+		apiError(resp, http.StatusInternalServerError, err.Error())
 		return
 	}
+	// Push API response data into our weather struct
 	weather.WeatherInfo = darkskyData
 
 	// JSON-ify our completed weather info object
 	jsonResp, err := json.Marshal(weather)
 	if err != nil {
-		http.Error(resp, err.Error(), http.StatusInternalServerError)
+		apiError(resp, http.StatusInternalServerError, err.Error())
 		return
 	}
+
 	// Fire JSON result back down the internet tubes
-	resp.Header().Set("Content-Type", "application/json")
 	resp.Write(jsonResp)
+}
+
+//
+// Helper function for returning API errors
+//
+func apiError(resp http.ResponseWriter, code int, message string) {
+	resp.WriteHeader(code)
+
+	//message = strings.ReplaceAll(message, "\"", "'")
+	errorData := &HTTPError{
+		Error: message,
+	}
+
+	errorResp, err := json.Marshal(errorData)
+	if err != nil {
+		fmt.Printf("### ERROR! httpError unable to marshal to JSON. Message was %s\n", message)
+		return
+	}
+	resp.Write(errorResp)
 }
